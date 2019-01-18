ec419cac9fce29ebb36aff55a86881d0dbcb2cbc
emarrs <marrsec@gmail.com>
2019-01-18 14:06:56 -0500

    remove tb

--- Twitter_Bot/auth.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'ZgeBOqe50aZWEuPu6lxPLDmGs'
-consumer_secret = 'w4T66O9J1Yn2TW7sgBgrWismzvxZVZjlMMVFk2ArMdrVEZJsZ7'
-access_token = '1082934552841121792-V14vwn4REdjGAlkyLap62WsO3jSLm6'
-access_token_secret = 'Af8rjsctvYkz1SZYdC7QaRLlI8EcdNs8wGu4M63CxQIqH'
