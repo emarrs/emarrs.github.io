0a10f310a1b72acb823c7a433f09201d9c55df30
emarrs <marrsec@gmail.com>
2019-02-01 18:40:27 -0500

    fix

--- breakup/techbro.js
+++ breakup/techbro.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       soPhone.innerHTML = sigName;
       message1.innerHTML = "the last " + timeTogether + " has been rad. unfortunately though, i think we should stop seeing each other.";
       message2.innerHTML = "it's not you, you're great. but i just cannot get my mind off of " +  objectsLike + " lately. it just stimulates my mind unlike any human ever could. also, i'm just so focused on work. my mind is full of app ideas, like an app that " + appIdeas + ". wouldn't that be sick? also, chilling with you has been really cutting in to my " + hobbiesLike + " time, and my productivity is decreasing.";
-      message3.innerHTML = "i knew i had to end things during my " + benderTime + "-long " + drugName + " bender, when i had the idea for an app where you can order " + orderProduct + " delivered to your door " + subTime + " upon subscription. this venture is going to take a lot of focus, and i cannot be tied down on my " + transport + " to success. ";
+      message3.innerHTML = "i knew i had to end things during my " + benderTime + "-week-long " + drugName + " bender, when i had the idea for an app where you can order " + orderProduct + " delivered to your door " + subTime + " upon subscription. this venture is going to take a lot of focus, and i cannot be tied down on my " + transport + " to success. ";
       message4.innerHTML = "i’m sorry to break your poor human heart. i look forward to seeing your status updates on my " + animalName + "-owner social networking app, " + animalName + "Mommy!" ;
       message5.innerHTML = "tap you later," + myName ;
 
