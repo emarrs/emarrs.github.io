dec002b0de5d8d8ba4ae438be28697b875c944f2
Libby Marrs <libby@libby.fios-router.home>
2020-08-17 15:27:19 -0400

    completely changed file path system on everything pray for this push

--- myrenaissanceface/script.js
+++
@@ -1,5 +0,0 @@
-function randomImg(){
-    var randomNumber = Math.floor(Math.random() * 10) + 1;
-    var imgName = "img_" + randomNumber;
-    document.getElementById("face").src =  +"faces/" + imgName + ".jpg";
-}
