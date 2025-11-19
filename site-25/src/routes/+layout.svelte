a6720a759baa26362bb48918719efa7d50c82fc5
Libby Marrs <marrsec@gmail.com>
2025-11-19 14:22:15 -0700

    move favicon and cname files

--- site-25/src/routes/+layout.svelte
+++ site-25/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import "../app.css";
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.svg" />
 </svelte:head>
 
 {@render children()}
