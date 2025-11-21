4b36d4daf3400fd202c75b67ffc8109c3a12f188
Libby Marrs <marrsec@gmail.com>
2025-11-21 15:10:59 -0700

    scroll smooth

--- site-25/src/routes/+page.svelte
+++ site-25/src/routes/+page.svelte
@@ -10,11 +10,10 @@
     <Header />
 
     <section class="flex h-full">
-        <!-- <section class="w-60 bg-amber-100 p-4"></section> -->
-        <section class="w-full space-y-4 p-4 overflow-y-scroll">
+        <section class="w-full space-y-4 p-4 overflow-y-scroll scroll-smooth">
 
             <section class="pt-24 flex flex-col-reverse lg:items-start lg:justify-between lg:flex-row xl:h-full mb-20">
-                <div class="font-serif max-w-100 flex flex-col gap-10 xl:h-full xl:justify-between pb-10 ">
+                <div id="top" class="font-serif max-w-100 flex flex-col gap-10 xl:h-full xl:justify-between pb-10 ">
                     <p class=" text-[#8a9b8a]">Currently working on interfaces for understanding complex data at <a href="https://www.fathom.info/" class="text-[#83c900]">Fathom Information Design</a>. Most of the images you'll find on this site (which I built myself!) are from graphic design, web, and writing projects I've done for fun. <span class="italic">Curious?</span> Email me to see more of my work or inquire about freelance availability. </p>
                     <div class="text-[#8f9b79] space-y-2">
                         <p class="italic font-light">All-time favorite projects:</p>
