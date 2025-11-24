3e8467a71560c4fea551409e4767d91e803fc264
Libby Marrs <marrsec@gmail.com>
2025-11-24 09:03:09 -0700

    colored sections

--- site-25/src/lib/components/Project.svelte
+++ site-25/src/lib/components/Project.svelte
@@ -4,11 +4,21 @@
     export let desc = "";
     export let link = "";
     export let cls = "";
+    export let color = "";
 </script>
 
-<section class="h-80 md:h-200 w-full pr-4 flex overflow-x-scroll overscroll-x-none scrollbar {cls}" id={id}>
+<section class={`pt-4 pb-8 h-92 md:h-212 w-full pr-4 flex overflow-x-scroll overscroll-x-none scrollbar bg-linear-to-br ${cls} 
+    ${color === 'blue' ? 'from-[#f4fcf0] to-[#f3fce0] text-[#73979b]' 
+    : color === 'yellow' ? 'from-[#fffce2] to-[#ffefef]'
+    : color === 'white' ? 'from-white to-[#ffefef] text-[#73979b]'
+    : 'bg-transparent text-amber-600'}`}
+id={id}>
     {#if title}
-    <div class="hidden md:block w-60 bg-amber-50 grow-0 shrink-0 h-max sticky left-0 font-serif text-amber-600 p-4 ">
+    <div class={`hidden md:block w-60 grow-0 shrink-0 h-max sticky left-0 font-serif p-4 pl-8 
+        ${color === 'blue' ? 'bg-[#f4fcf0]' 
+        : color === 'yellow' ? 'bg-[#fffce2]'
+        : color === 'white' ? 'bg-white'
+        : 'bg-[#fffce2]'}`}>
         <h1 class="uppercase text-sm leading-4 mb-2 font-light"> {title} </h1>
         <p class="text-sm italic"> {@html desc } </p>
          {#if link}<a class="block mt-2 text-sm italic text-[#7f7fbc] hover:text-[#4949ae]" href={link} target="_blank">Live site</a>{/if}
@@ -16,7 +26,7 @@
        
     {/if}
     
-    <div class="h-80 md:h-200">
+    <div class="h-80 md:h-200 pl-4">
         <div class="h-full flex gap-2 w-max">
             <slot></slot>
         </div>
--- site-25/src/routes/+page.svelte
+++ site-25/src/routes/+page.svelte
@@ -10,10 +10,10 @@
     <Header />
 
     <section class="flex h-full">
-        <section class="w-full space-y-4 p-4 pr-0 pt-8 overflow-y-scroll scroll-smooth">
+        <section class="w-full pt-8 overflow-y-scroll scroll-smooth">
 
             <section class="gap-8 flex flex-col-reverse items-center lg:flex-row lg:items-start lg:justify-between lg:h-full lg:max-h-170 xl:max-h-200 mb-20">
-                <div id="top" class="py-10 pr-4 lg:px-4 pb-10 font-serif max-w-120 lg:max-w-80 flex flex-col gap-10 h-full justify-between text-sm">
+                <div id="top" class="py-10 px-4 lg:px-8 pb-10 font-serif max-w-120 lg:max-w-80 flex flex-col gap-10 h-full justify-between text-sm">
                     <div>
                         <p class=" text-[#8a9b8a]">Currently working on interfaces for understanding complex data at <a href="https://www.fathom.info/" class="text-[#83c900]">Fathom Information Design</a>. Most of the images you'll find on this site (which I built myself!) are from graphic design, web, and writing projects I've done for fun. <span class="italic">Curious?</span> Reach out to see more of my work or inquire about freelance availability. </p>
                         <ul class="mt-4">
@@ -48,7 +48,8 @@
             
             <Project 
                 title = "Album artwork"
-                desc = "For Moses Archuleta and Anastasia Coope"
+                desc = "For Moses Archuleta and Anastasia Coope" 
+                color = "blue"
             >
                 <Image src="cemeteryclassics/vinyl-group.png" />
                 <Image src="cemeteryclassics/cover-f-final.png" />
@@ -59,7 +60,7 @@
                 <Image src="albums-other/toughsun-vinyl.jpg" />
             </Project>
 
-            <Project >
+            <Project color = "blue">
                 <Image src="albums-other/1.png" />
                 <div class="grid grid-cols-3 w-max max-w-max gap-2 grid-rows-2 max-h-full">
                     <Image src="albums-other/231215D.png" />
@@ -92,7 +93,7 @@
 
             <Project
                 title = "Post-Authentic Sincerity"
-                desc = 'For my Graphic Design degree project at RISD, I wrote an essay and built a website to contain it. The writing explores realness, nature, and default systems in the context of branding and graphic design, and how recent shifts in the way these ideas are symbolized and marketed indicate a cultural movement away from "authenticity". The essay also argues for a "post-authentic" approach to graphic design discourse which acknowledges its own function as commodity service, rather than an artform inherently independent of commerce.'
+                desc = 'For my Graphic Design degree project at RISD, I wrote an essay and built a website to contain it. The writing explores realness, nature, and default systems in the context of branding and graphic design, and how recent shifts in the way these ideas are symbolized and marketed indicate a cultural movement away from "authenticity".'
                 link = "https://libbymarrs.net/post-authentic-sincerity/"
                 id = "post-authentic-sincerity"
             >
@@ -103,6 +104,7 @@
             <Project 
                 title = "A wedding invitation"
                 desc = "For some friends"
+                color = "blue"
             >
                 <Image src="wedding/menu.png"  cls="md:-ml-60" />
                 <Image src="wedding/cards_sm.png" />
@@ -123,8 +125,9 @@
                 id = "ron-carter-universe"
                 title = "Ron Carter Universe"
                 desc = "Ron Carter is probably the most legendary bassist in jazz history. In honor of the vibrance and elegance of his life's work, I designed and built an immersive discography with the help of Mr. Carter himself and my team at Fathom. <br>
-                I wrote a series of blog posts reflecting on the project <a class='text-[#7f7fbc] hover:text-[#4949ae]' hover:text-'' href='#'>here,</a> <a class='text-[#7f7fbc] hover:text-[#4949ae]' hover:text-'' href='#'>here,</a> and <a class='text-[#7f7fbc] hover:text-[#4949ae]' href='#'>here</a>."
+                I wrote a series of blog posts reflecting on the project <a target='_blank' class='text-[#7f7fbc] hover:text-[#4949ae]' href='https://www.fathom.info/notebook/231108/'>here</a>, <a target='_blank' class='text-[#7f7fbc] hover:text-[#4949ae]' href='https://www.fathom.info/notebook/231115/'>here</a>, and <a target='_blank' class='text-[#7f7fbc] hover:text-[#4949ae]' href='https://www.fathom.info/notebook/231127/'>here</a>."
                 link = "https://www.roncarteruniverse.com/"
+                color = "blue"
             >
                 <Image src="roncarter/desktop-photo.png" />
                 <Image src="roncarter/sc1.jpg" />
@@ -139,6 +142,7 @@
              <Project 
                 title = "Island Time"
                 desc = "Book cover design with custom, hand-drawn lettering"
+                color = "white"
             >
                 <div class="flex">
                     <!-- <Image src="islandtime/cover-crop.png" /> -->
